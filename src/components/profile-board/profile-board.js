a67950229d0c5ee9963731f48db8d98609653b46
Dmitry Kovalev <dimakov7@gmail.com>
2022-03-21 15:17:58 +0300

    init: img-collection

--- src/components/profile-board/profile-board.js
+++ src/components/profile-board/profile-board.js
@@ -2,9 +2,11 @@ let data = [];
 
 const boardList = document.querySelector(".drop-btn");
 const divMyDropdown = document.querySelector('.dropdown-content2');
+let divWrapperHead = document.querySelector('.popup-profile__wrapper-head')
 let divWrapperPopup = document.querySelector('.popup-profile__wrapper')
 const divRename = document.querySelector('.board-rename');
 let popupProfile = document.querySelector('.popup-profile');
+
 
 
 // рисую выподающий список исходя из количества элементов
@@ -15,10 +17,12 @@ const initList = () => {
         const divBoard = document.createElement('div');
         const imgBoard = document.createElement('div');
         imgBoard.classList.add('board-img');
+        
         divWrapper.setAttribute('data-id', element.id);
         divWrapper.classList.add('board-wrapper')
         divBoard.setAttribute('data-id', element.id);
         popupProfile.setAttribute('data-id', element.id);
+        // imgBoard.classList.setAttribute('data-id', element.id);
         // const img1 = element.imgLogo;
         imgBoard.style.background = element.imageLogo;
         divBoard.append(element.name);
@@ -74,14 +78,54 @@ const showBoardProfile = (boardId) => {
 
 //ФУНКЦИЯ Рисую доску по ID в popup
 let boardProfile = (profileId) => {
+    divWrapperHead.innerHTML = '';
+    
     const imgLogoPopup = document.createElement('div'); // лого
     const titlePopup = document.createElement('div'); // Заголовок
+    // const imgWrapper = document.createElement('div'); // обвертка для коллекции картинок
     imgLogoPopup.classList.add('board-img-popup');
-    imgLogoPopup.style.background = profileId.imageLogo;
+    imgLogoPopup.style.backgroundImage = profileId.imageLogo;
     titlePopup.classList.add('popup-profile__title');
+    // imgWrapper.classList.add('imgWrapper');
     titlePopup.append(profileId.name);
-    divWrapperPopup.append(titlePopup);
+    titlePopup.append(btnRename);
+    divWrapperHead.prepend(titlePopup);  //!!!!!!!!!!!!!
+    divWrapperHead.append(divWrapperPopup);
     divWrapperPopup.append(imgLogoPopup);
+    const imgWrapper = document.querySelector('.imgWrapper')
+    // imgWrapper.innerHTML = '';
+    
+    // popupProfile.append(imgWrapper);
+console.log(btnRename);
+
+
+    // отрисовка ПИНОВ
+const declination = ['пин', 'пина', 'пинов']
+function getDeclination(number, txt) {
+        let cases = [2, 0, 1, 1, 1, 2];
+        return txt[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+    }
+
+
+const numberPin = document.querySelector('.number-pin')
+numberPin.innerHTML = '';
+const pin = getDeclination(profileId.img.length, declination)
+console.log(numberPin);
+const nubmer = profileId.img.length
+numberPin.append(nubmer, ' ' ,pin);
+console.log( profileId.img.length);
+    profileId.img.forEach(element => {
+
+        const imgCard = document.createElement('div');
+        imgCard.classList.add('imgCard');
+        imgCard.style.background = element;
+        // imgCard.append(element);
+        imgWrapper.append(imgCard);
+
+    let height = Math.round(Math.random() * (400 - 320) + 320);
+    imgCard.style.height = height + 'px';
+
+    });
 
 }
 // ==================================================
@@ -156,7 +200,7 @@ const renameBtnOK = modalProfile.querySelector('.board-rename__ok');
 renameBtnClose.addEventListener('click', () => {
 
     divRename.style.display = "none";
-    document.querySelector("body").style.overflow = "auto";
+    // document.querySelector("body").style.overflow = "auto";
 
 });
 
